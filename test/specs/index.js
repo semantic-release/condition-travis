7f2781e84bee75ab68daf9bda7d25c44e205d470
Stephan Bönnemann <stephan@boennemann.me>
2015-07-14 12:10:28 +0200

    test: initial

---
+++ test/specs/index.js
@@ -0,0 +1,100 @@
+const { test } = require('tap')
+const SRError = require('@semantic-release/error')
+
+const condition = require('../../dist')
+
+test('raise errors in travis environment', (t) => {
+  t.test('only runs on travis', (tt) => {
+    tt.plan(2)
+
+    condition({}, {}, {}, {}, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'ENOTRAVIS')
+    })
+  })
+
+  t.test('not running on pull requests', (tt) => {
+    tt.plan(2)
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_PULL_REQUEST: true
+    }, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EPULLREQUEST')
+    })
+  })
+
+  t.test('not running on tags', (tt) => {
+    tt.plan(2)
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_TAG: true
+    }, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EGITTAG')
+    })
+  })
+
+  t.test('only running on specified branch', (tt) => {
+    tt.plan(5)
+
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_BRANCH: 'master'
+    }, (err) => {
+      tt.is(err, null)
+    })
+
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_BRANCH: 'notmaster'
+    }, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EBRANCHMISMATCH')
+    })
+
+    condition({}, {
+      release: {
+        branch: 'foo'
+      }
+    }, {}, {
+      TRAVIS: true,
+      TRAVIS_BRANCH: 'master'
+    }, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EBRANCHMISMATCH')
+    })
+  })
+
+  t.test('supports travis_after_all', (tt) => {
+    tt.plan(5)
+
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_BRANCH: 'master',
+      BUILD_LEADER: 'YES',
+      BUILD_AGGREGATE_STATUS: 'others_succeeded'
+    }, (err) => {
+      tt.is(err, null)
+    })
+
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_BRANCH: 'master',
+      BUILD_LEADER: 'NO'
+    }, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'ENOBUILDLEADER')
+    })
+
+    condition({}, {}, {}, {
+      TRAVIS: true,
+      TRAVIS_BRANCH: 'master',
+      BUILD_LEADER: 'YES',
+      BUILD_AGGREGATE_STATUS: 'others_failed'
+    }, (err) => {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EOTHERSFAILED')
+    })
+  })
+})
