16dffd64040555fc18dc0528a79b68bf346d5f07
Stephan Bönnemann <stephan@boennemann.me>
2015-10-26 13:17:51 +0000

    fix: make error messages meaningful

--- src/index.js
+++ src/index.js
@@ -1,22 +1,60 @@
 const travisAfterAll = require('travis-after-all')
 
+const semver = require('semver')
 const SRError = require('@semantic-release/error')
 
 module.exports = function (pluginConfig, {env, options}, cb) {
-  if (env.TRAVIS !== 'true') return cb(new SRError('Not running on Travis', 'ENOTRAVIS'))
+  if (env.TRAVIS !== 'true') {
+    return cb(new SRError(
+      'semantic-release didn’t run on Travis CI and therefore a new version won’t be published.\n' +
+      'You can customize this behavior using "verifyConditions" plugins: git.io/sr-plugins',
+      'ENOTRAVIS'
+    ))
+  }
 
-  if (env.hasOwnProperty('TRAVIS_PULL_REQUEST') && env.TRAVIS_PULL_REQUEST !== 'false') return cb(new SRError('Not publishing from pull requests', 'EPULLREQUEST'))
-  if (env.TRAVIS_TAG) return cb(new SRError('Not publishing from tags', 'EGITTAG'))
+  if (env.hasOwnProperty('TRAVIS_PULL_REQUEST') && env.TRAVIS_PULL_REQUEST !== 'false') {
+    return cb(new SRError(
+      'This test run was triggered by a pull request and therefore a new version won’t be published.',
+      'EPULLREQUEST'
+    ))
+  }
 
-  if (options.branch !== env.TRAVIS_BRANCH) return cb(new SRError(`Branch is not ${options.branch}`, 'EBRANCHMISMATCH'))
+  if (env.TRAVIS_TAG) {
+    let errorMessage = 'This test run was triggered by a git tag and therefore a new version won’t be published.'
+
+    if (semver.valid(env.TRAVIS_TAG)) {
+      errorMessage += '\nIt is very likely that this tag was created by semantic-release itself.\n' +
+       'Everything is okay. For log output of the actual publishing process look at the build that ran before this one.'
+    }
+
+    return cb(new SRError(errorMessage, 'EGITTAG'))
+  }
+
+  if (options.branch !== env.TRAVIS_BRANCH) {
+    return cb(new SRError(
+      `This test run was triggered on the branch ${env.TRAVIS_BRANCH}, while semantic-release is configured to only publish from ${options.branch}.\n` +
+      'You can customize this behavior using the "branch" option: git.io/sr-options',
+      'EBRANCHMISMATCH'
+    ))
+  }
 
   travisAfterAll((code, err) => {
     if (code === 0) return cb(null)
 
-    if (code === 1) return cb(new SRError('Not publishing when other jobs in the build matrix fail.', 'EOTHERSFAILED'))
+    if (code === 1) {
+      return cb(new SRError(
+        'In this test run not all jobs passed and therefore a new version won’t be published.',
+        'EOTHERSFAILED'
+      ))
+    }
 
-    if (code === 2) return cb(new SRError('Not publishing from minion', 'ENOBUILDLEADER'))
+    if (code === 2) {
+      return cb(new SRError(
+        'This test run is not the build leader and therefore a new version won’t be published.',
+        'ENOBUILDLEADER'
+      ))
+    }
 
-    cb(err || new SRError('travis-after-all return unexpected error code', 'ETAAFAIL'))
+    cb(err || new SRError('travis-after-all returned unexpected error code', 'ETAAFAIL'))
   })
 }
