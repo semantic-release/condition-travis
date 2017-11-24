1f7794443155a6e8c2b39e305a2ceead6fdbfb3e
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-11-23 22:08:24 -0500

    feat: Return async functions
    BREAKING CHANGE: Return async functions instead of functions calling a callback

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ const GitHubApi = require('github');
 const parseSlug = require('parse-github-repo-url');
 const semver = require('semver');
 const deployOnce = require('travis-deploy-once');
-const SRError = require('@semantic-release/error');
+const SemanticReleaseError = require('@semantic-release/error');
 const resolveConfig = require('./lib/resolve-config');
 
-module.exports = async function(pluginConfig, {pkg, env, options: {branch} = {}}, callback) {
+module.exports = async function(pluginConfig, {pkg, env, options: {branch} = {}}) {
   const {githubToken, githubUrl, githubApiPathPrefix} = resolveConfig(pluginConfig);
   if (env.TRAVIS !== 'true') {
-    return callback(
-      new SRError(
-        'semantic-release didn’t run on Travis CI and therefore a new version won’t be published.\nYou can customize this behavior using "verifyConditions" plugins: git.io/sr-plugins',
-        'ENOTRAVIS'
-      )
+    throw new SemanticReleaseError(
+      'semantic-release didn’t run on Travis CI and therefore a new version won’t be published.\nYou can customize this behavior using "verifyConditions" plugins: git.io/sr-plugins',
+      'ENOTRAVIS'
     );
   }
 
   if (Object.prototype.hasOwnProperty.call(env, 'TRAVIS_PULL_REQUEST') && env.TRAVIS_PULL_REQUEST !== 'false') {
-    return callback(
-      new SRError(
-        'This test run was triggered by a pull request and therefore a new version won’t be published.',
-        'EPULLREQUEST'
-      )
+    throw new SemanticReleaseError(
+      'This test run was triggered by a pull request and therefore a new version won’t be published.',
+      'EPULLREQUEST'
     );
   }
 
@@ -34,59 +30,45 @@ module.exports = async function(pluginConfig, {pkg, env, options: {branch} = {}}
         '\nIt is very likely that this tag was created by semantic-release itself.\nEverything is okay. For log output of the actual publishing process look at the build that ran before this one.';
     }
 
-    return callback(new SRError(errorMessage, 'EGITTAG'));
+    throw new SemanticReleaseError(errorMessage, 'EGITTAG');
   }
 
   if (branch !== env.TRAVIS_BRANCH) {
-    return callback(
-      new SRError(
-        `This test run was triggered on the branch ${
-          env.TRAVIS_BRANCH
-        }, while semantic-release is configured to only publish from ${
-          branch
-        }.\nYou can customize this behavior using the "branch" option: git.io/sr-options`,
-        'EBRANCHMISMATCH'
-      )
+    throw new SemanticReleaseError(
+      `This test run was triggered on the branch ${
+        env.TRAVIS_BRANCH
+      }, while semantic-release is configured to only publish from ${
+        branch
+      }.\nYou can customize this behavior using the "branch" option: git.io/sr-options`,
+      'EBRANCHMISMATCH'
     );
   }
 
-  let result;
-  try {
-    const {port, protocol, hostname} = githubUrl ? url.parse(githubUrl) : {};
-    const github = new GitHubApi({
-      port,
-      protocol: (protocol || '').split(':')[0] || null,
-      host: hostname,
-      pathPrefix: githubApiPathPrefix || null,
-    });
-    const [owner, repo] = parseSlug(pkg.repository.url);
+  const {port, protocol, hostname} = githubUrl ? url.parse(githubUrl) : {};
+  const github = new GitHubApi({
+    port,
+    protocol: (protocol || '').split(':')[0] || null,
+    host: hostname,
+    pathPrefix: githubApiPathPrefix || null,
+  });
+  github.authenticate({type: 'token', token: githubToken});
 
-    github.authenticate({type: 'token', token: githubToken});
+  const [owner, repo] = parseSlug(pkg.repository.url);
+  const {data: {private: pro}} = await github.repos.get({owner, repo});
 
-    const {data: {private: pro}} = await github.repos.get({owner, repo});
-
-    result = await deployOnce({travisOpts: {pro}});
-  } catch (err) {
-    return callback(err);
-  }
+  const result = await deployOnce({travisOpts: {pro}});
 
   if (result === null) {
-    return callback(
-      new SRError(
-        'This test run is not the build leader and therefore a new version won’t be published.',
-        'ENOBUILDLEADER'
-      )
+    throw new SemanticReleaseError(
+      'This test run is not the build leader and therefore a new version won’t be published.',
+      'ENOBUILDLEADER'
     );
   }
 
   if (result === false) {
-    return callback(
-      new SRError(
-        'In this test run not all jobs passed and therefore a new version won’t be published.',
-        'EOTHERSFAILED'
-      )
+    throw new SemanticReleaseError(
+      'In this test run not all jobs passed and therefore a new version won’t be published.',
+      'EOTHERSFAILED'
     );
   }
-
-  callback(null);
 };
